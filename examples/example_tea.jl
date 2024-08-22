a0c7a44bb790c57598cff6099eed06ec85c86cfe
Matteo Baù <bau@rse-web.it>
2024-08-22 17:17:41 +0200

    Fix typo

--- examples/example_tea.jl
+++ examples/example_tea.jl
@@ -22,7 +22,7 @@ path2grid = joinpath(_HWTEA_dir, "test/data", "grids/adcdc", "case39_mcdc.m")
 path2data = joinpath(_HWTEA_dir, "test/data", "timeseries", "example_mc")
 setting = Dict("output" => Dict("branch_flows" => true, "duals" =>false), "conv_losses_mp" => false);
 setting_opt = Dict("solver" => "ipm", "ipm_iteration_limit" => 3000, "time_limit" => 3600.0, "output_flag" => false)
-optimizer = HVDCWISE_TEA.optimizer_with_attributes(HiGHS.Optimizer, setting_opt)
+optimizer = HVDCWISE_TEA.optimizer_with_attributes(HiGHS.Optimizer, setting_opt...)
 
 
 ## Solve the multiperiod OPF problem
