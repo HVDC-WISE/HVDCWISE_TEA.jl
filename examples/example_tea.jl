37fc9002df1f0a4cdd3d0eb6448a7716529be8ef
Matteo Baù <bau@rse-web.it>
2024-09-03 11:44:53 +0200

    Update `HiGHS` solver settings

--- examples/example_tea.jl
+++ examples/example_tea.jl
@@ -21,7 +21,7 @@ hours = 168
 path2grid = joinpath(_HWTEA_dir, "test/data", "grids/adcdc", "case39_mcdc.m")
 path2data = joinpath(_HWTEA_dir, "test/data", "timeseries", "example_mc")
 setting = Dict("output" => Dict("branch_flows" => true, "duals" =>false), "conv_losses_mp" => false);
-setting_opt = Dict("solver" => "ipm", "ipm_iteration_limit" => 3000, "time_limit" => 3600.0, "output_flag" => false)
+setting_opt = Dict("presolve" => "on", "solver" => "ipm", "run_crossover" => "off", "ipm_iteration_limit" => 3000, "time_limit" => 3600.0, "output_flag" => false)
 optimizer = HVDCWISE_TEA.optimizer_with_attributes(HiGHS.Optimizer, setting_opt...)
 
 
